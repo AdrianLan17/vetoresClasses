62c0e3501344236b716fa6e1f88ac4bc66e37799
Adrian <adrian.chirinos@etec.sp.gov.br>
2024-09-23 16:48:24 -0300

    adicao

---
+++ cadastro/Paciente.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Paciente: Pessoa
+    {
+        string[] tipoConsult = new string[10];
+        string[] sintomas = new string[10];
+        string[] tempoSintomas = new string[10];
+        string[] tempCorp = new string[10];
+        string[] classifcRisc  = new string[10];
+        int i;
+        string ope;
+
+        public override void cadastro()
+        {
+
+            for (i = 0; i < 10; i++)
+            {
+                base.cadastro();
+                Console.WriteLine("digite se é uma consulta particular ou convenio");
+                tipoConsult[i] = Console.ReadLine();
+                Console.WriteLine("sintomas que o paciente esta sentindo:");
+                sintomas[i] = Console.ReadLine();
+                Console.WriteLine("Digite o tempo que o paciente está sentindo esses sintomas:");
+                tempoSintomas[i] = Console.ReadLine();
+                Console.WriteLine("Digite a temperatura corporal do paciente");
+                tempCorp[i] = Console.ReadLine();
+                Console.WriteLine("digite a classificaçao de risco do paciente");
+                classifcRisc[i] = Console.ReadLine();
+
+                Console.WriteLine("Digite q para sair!!");
+                ope= Console.ReadLine();
+                if(ope == "q")
+                {
+                    break;
+                }
+
+            }
+        }
+
+        
+
+
+
+    }
+}
---
+++ cadastro/Pessoa.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Pessoa
+        
+    {
+        string[] nome = new string[10];
+        string[] cpf = new string[10];
+        string[] numeroTel = new string[10];
+        string[] email = new string[10];
+        string[] dataNasc = new string[10];
+        string[] cep = new string[10];
+        string[] rua = new string[10];
+        string[] bairro = new string[10];
+        string[] cidade = new string[10];
+        int[] numeroCasa = new int[10];
+        int[] idade = new int[10];
+        int i;
+        public virtual void cadastro()
+        {
+                    Console.WriteLine("CADASTRO!\n");
+                    Console.WriteLine("Digite o nome do pasciente:");
+                    this.nome[i] = Console.ReadLine();
+                    Console.WriteLine("digite seu cpf");
+                    this.cpf[i] = Console.ReadLine();
+                    Console.WriteLine("digite seu numero de telefone");
+                    this.numeroTel[i] = Console.ReadLine();
+                    Console.WriteLine("digite seu email");
+                    this.email[i] = Console.ReadLine();
+                    Console.WriteLine("digite sua data de nascimento");
+                    this.dataNasc[i] = Console.ReadLine();
+                    Console.WriteLine("digite a sua idade");
+                    this.idade[i] = int.Parse(Console.ReadLine());
+                    Console.WriteLine("digite seu cep");
+                    this.cep[i] = Console.ReadLine();  
+                    Console.WriteLine("digite o nome da sua rua");
+                    this.rua[i] = Console.ReadLine();
+                    Console.WriteLine("digite o nome do seu bairro");
+                    this.bairro[i] = Console.ReadLine();
+                    Console.WriteLine("digite o nome da sua cidade");
+                    this.cidade[i] = Console.ReadLine();
+                    Console.WriteLine("digite o numero da casa");
+                    this.numeroCasa[i]= int.Parse(Console.ReadLine());
+        
+                      
+
+                
+
+            
+        }
+      
+    }
+}
